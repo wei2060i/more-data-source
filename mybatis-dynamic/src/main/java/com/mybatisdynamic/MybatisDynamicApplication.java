0ced7ccce1c0881de8d42c7fed8aa7cedcd392d5
yueweiwei <2422184607@qq.com>
2021-07-02 23:16:16 +0800

    mybatis项目搭建

---
+++ mybatis-dynamic/src/main/java/com/mybatisdynamic/MybatisDynamicApplication.java
@@ -0,0 +1,16 @@
+package com.mybatisdynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wei
+ */
+@SpringBootApplication
+public class MybatisDynamicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisDynamicApplication.class, args);
+    }
+
+}
---
+++ mybatis-dynamic/src/test/java/com/mybatisdynamic/MybatisDynamicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatisdynamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisDynamicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
