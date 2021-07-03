26e26d19588ac55176d1cfb9fa121aa71ff5742a
yueweiwei <2422184607@qq.com>
2021-07-03 19:20:47 +0800

    代码生成工具

--- mybatis-dynamic/src/main/java/com/mybatisdynamic/MybatisDynamicApplication.java
+++ mybatis-dynamic/src/main/java/com/mybatisdynamic/MybatisDynamicApplication.java
@@ -2,11 +2,12 @@ package com.mybatisdynamic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author Wei
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MybatisDynamicApplication {
 
     public static void main(String[] args) {
