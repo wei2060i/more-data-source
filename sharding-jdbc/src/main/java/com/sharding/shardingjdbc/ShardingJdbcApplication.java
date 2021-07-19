304ddc272e4cfb5194c4ec961b6e9d45bfeeffb5
yueweiwei <2422184607@qq.com>
2021-07-19 10:25:36 +0800

    sharding

---
+++ sharding-jdbc/src/main/java/com/sharding/shardingjdbc/ShardingJdbcApplication.java
@@ -0,0 +1,16 @@
+package com.sharding.shardingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wei
+ */
+@SpringBootApplication
+public class ShardingJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcApplication.class, args);
+    }
+
+}
